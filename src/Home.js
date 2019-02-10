e4ae070603120653344e2b00a4c842f20c2b99c3
Viveksanthosh <viveksanthosh@holidayiq.com>
2019-02-10 22:17:46 +0530

    added mutate dogs

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 
 import './Home.css';
-import { Query } from 'react-apollo';
+import { Query, Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 
-const Show = ({ dog }) => <Query query={
+const ShowDog = ({ dog }) => <Query query={
   gql`
       query Dog($dog: Int!) {
          dog(id: $dog){
@@ -21,17 +21,44 @@ const Show = ({ dog }) => <Query query={
   }
 </Query>
 
+const nameRef = React.createRef();
+const idRef = React.createRef();
+const owneridRef = React.createRef();
 
-const Home = () => {
+const AddDog = () => <Mutation mutation={gql`
+mutation AddDog($name: String!, $id: Int!, $ownerid: Int!){
+  addDog(name: $name, id: $id, ownerid: $ownerid){
+    name
+  }
+}
+`}>{(AddDog) =>
+    <div>
+      <h2>Add Dog</h2>
+      <input ref={nameRef} placeholder='name' />
+      <input ref={idRef} placeholder='id' />
+      <input ref={owneridRef} placeholder='ownerid' />
+      <button onClick={() => {
+        AddDog({ variables: { name: nameRef.current.value, id: idRef.current.value*1, ownerid: owneridRef.current.value*1 } })
+      }}>Submit</button>
+    </div>
+  }
+</Mutation>
+
+
+const Main = () => {
   const [dogId, setDogId] = useState(1)
 
   return <div>
     <input value={dogId} onChange={(e) => setDogId(Number(e.target.value))} />
     <br />
-    <Show dog={dogId} />
+    <ShowDog dog={dogId} />
+
+    <br />
+    <AddDog />
+
   </div>
 }
 
 
 
-export default Home;
+export default Main;
